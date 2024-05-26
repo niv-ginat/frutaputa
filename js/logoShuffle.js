a8125f24ec7935fc556df2043989e8ec5aefc8e2
niv-ginat <niv.ginat@gmail.com>
2024-05-26 22:22:22 +0200

    Cleaned folder

--- js/logoShuffle.js
+++ js/logoShuffle.js
@@ -2,34 +2,12 @@
 
 // Array of image URLs for the logo collection thumbnails
 const logoCollectionImages = [
-    "images/logo-shuffle/logo_apm.jpg",
-    "images/logo-shuffle/logo_astral.jpg",
-    "images/logo-shuffle/logo_CHFP.jpg",
-    "images/logo-shuffle/logo_dreamsociety.jpg",
-    "images/logo-shuffle/logo_funbox.jpg",
-    "images/logo-shuffle/logo_m-logo.jpg",
-    "images/logo-shuffle/logo_rosel.jpg",
-    "images/logo-shuffle/logo_the-lab.jpg",
-    "images/logo-shuffle/logo_tradie.jpg",
-    "images/logo-shuffle/logo_bouklas.jpg",
-    "images/logo-shuffle/logo_cherry.jpg",
-    "images/logo-shuffle/logo_continuum",
-    "images/logo-shuffle/logo_dafne.jpg",
-    "images/logo-shuffle/logo_fiona.jpg",
-    "images/logo-shuffle/logo_ii.jpg",
-    "images/logo-shuffle/logo_intrunk.jpg",
-    "images/logo-shuffle/logo_lazoo.jpg",
-    "images/logo-shuffle/logo_m-logo.jpg",
-    "images/logo-shuffle/logo_master.jpg",
-    "images/logo-shuffle/logo_OC2019.jpg",
-    "images/logo-shuffle/logo_open.jpg",
-    "images/logo-shuffle/logo_solace.jpg",
-    "images/logo-shuffle/logo_squareone.jpg",
-    "images/logo-shuffle/logo_sv.jpg",
-    "images/logo-shuffle/logo_nite-records.jpg",
-    "images/logo-shuffle/logo_carla.jpg",
-    "images/logo-shuffle/logo_ucla.jpg",
-    "images/logo-shuffle/logo_tradie.jpg",
+    "images/care-package/fp_carepackages_letitbe.jpg",
+    "images/care-package/fp_carepackages_hands.jpg",
+    "images/care-package/fp_carepackages_nuance.jpg",
+    "images/care-package/fp_carepackages_papaya.jpg",
+    "images/care-package/fp_carepackages_stillife.jpg",
+    
     
     
 ];
